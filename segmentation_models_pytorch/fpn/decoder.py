ddffa535b474393095413d678a9ecc759e29f2a8
qishen-ha <qishen.ha@linecorp.com>
2019-11-15 23:23:05 +0900

    hotpush

--- segmentation_models_pytorch/fpn/decoder.py
+++ segmentation_models_pytorch/fpn/decoder.py
@@ -32,6 +32,18 @@ def relu_fn(x):
     return swish_layer(x)
 
 
+class ConvBnRelu2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=(3,3), stride=(1,1), padding=(1,1)):
+        super(ConvBnRelu2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+    def forward(self, z):
+        x = self.conv(z)
+        x = self.bn(x)
+        x = relu_fn(x)
+        return x
+
+
 class Conv3x3GNReLU(nn.Module):
     def __init__(self, in_channels, out_channels, upsample=False):
 
